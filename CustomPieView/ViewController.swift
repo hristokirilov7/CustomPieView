65025059100d1136ad86b28f007df2be54d64cdb
Hristo Kirilov <hristo.kirilov@sap.com>
2017-11-27 20:13:53 +0200

    Initial Commit

---
+++ CustomPieView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomPieView
+//
+//  Created by Kirilov, Hristo on 27.11.17.
+//  Copyright © 2017 Kirilov, Hristo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
